af49762be466619f2067b728bf4de9e8d4379cbd
theRAZE001 <55403638+theRAZE001@users.noreply.github.com>
2021-03-09 10:41:32 +0530

    Update scoreBoard.py

--- scoreBoard.py
+++ scoreBoard.py
@@ -1,10 +1,12 @@
 from turtle import Turtle
-
+FONT = ("courier", 22, "normal")
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("highscore.txt") as data:
+            self.highscore = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.color("white")
@@ -12,11 +14,19 @@ class ScoreBoard(Turtle):
         self.update_scoreBoard()
 
     def update_scoreBoard(self):
-        self.write(f"Score:{self.score}", align="center", font=("courier", 22, "normal"))
+        self.clear()
+        self.write(f"Score:{self.score} Highscore: {self.highscore}", align="center", font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("courier", 22, "normal"))
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("highscore.txt", "w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreBoard()
+    #def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write("GAME OVER", align="center", font=FONT)
     def increase_score(self):
         self.score += 1
         self.clear()
